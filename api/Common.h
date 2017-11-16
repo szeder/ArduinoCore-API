cf53821b804830a7f10862b4eaa916ac38463bf1
Martino Facchin <m.facchin@arduino.cc>
2017-11-16 16:44:57 +0100

    [ATN] Add commonly used function
    check if they interfere with math.h or other headers

---
+++ api/Common.h
@@ -0,0 +1,144 @@
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void yield(void);
+
+typedef enum {
+  LOW  = 0x0,
+  HIGH = 0x1,
+} _pinStatus;
+
+typedef enum {
+  INPUT         = 0x0,
+  OUTPUT        = 0x1,
+  INPUT_PULLUP  = 0x2,
+  INPUT_PULLDOWN  = 0x3,
+} _pinMode;
+
+#define PI 3.1415926535897932384626433832795
+#define HALF_PI 1.5707963267948966192313216916398
+#define TWO_PI 6.283185307179586476925286766559
+#define DEG_TO_RAD 0.017453292519943295769236907684886
+#define RAD_TO_DEG 57.295779513082320876798154814105
+#define EULER 2.718281828459045235360287471352
+
+#define SERIAL  0x0
+#define DISPLAY 0x1
+
+typedef enum {
+  CHANGE = 1,
+  FALLING = 2,
+  RISING = 3,
+} _interruptMode;
+
+typedef enum {
+  LSBFIRST = 0,
+  MSBFIRST = 1,
+} _spi_bitFirst_mode;
+
+#ifndef min
+#define min(a,b) ((a)<(b)?(a):(b))
+#endif
+
+#ifndef max
+#define max(a,b) ((a)>(b)?(a):(b))
+#endif
+
+#ifndef abs
+#define abs(x) ((x)>0?(x):-(x))
+#endif
+
+#ifndef constrain
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#endif
+
+#ifndef round
+#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#endif
+
+#ifndef radians
+#define radians(deg) ((deg)*DEG_TO_RAD)
+#endif
+
+#ifndef degrees
+#define degrees(rad) ((rad)*RAD_TO_DEG)
+#endif
+
+#ifndef sq
+#define sq(x) ((x)*(x))
+#endif
+
+// interrupts() / noInterrupts() must be defined by the core
+
+#define lowByte(w) ((uint8_t) ((w) & 0xff))
+#define highByte(w) ((uint8_t) ((w) >> 8))
+
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+
+typedef unsigned int word;
+
+#ifndef bit
+#define bit(b) (1UL << (b))
+#endif
+
+typedef bool boolean;
+typedef uint8_t byte;
+
+void init(void);
+void initVariant(void);
+
+int atexit(void (*func)()) __attribute__((weak));
+int main() __attribute__((weak));
+
+void pinMode(uint8_t pinNumber, _pinMode pinMode);
+void digitalWrite(uint8_t pinNumber, _pinStatus status);
+_pinStatus digitalRead(uint8_t pinNumber);
+int analogRead(uint8_t pinNumber);
+void analogReference(uint8_t mode);
+void analogWrite(uint8_t pinNumber, int value);
+
+unsigned long millis(void);
+unsigned long micros(void);
+void delay(unsigned long);
+void delayMicroseconds(unsigned int us);
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout);
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+
+void attachInterrupt(uint8_t interruptNumber, void (*callback)(void), _interruptMode mode);
+void detachInterrupt(uint8_t interruptNumber);
+
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#ifdef __cplusplus
+
+/* C++ prototypes */
+uint16_t makeWord(uint16_t w);
+uint16_t makeWord(byte h, byte l);
+
+#define word(...) makeWord(__VA_ARGS__)
+
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
+
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0);
+void noTone(uint8_t _pin);
+
+// WMath prototypes
+long random(long);
+long random(long, long);
+void randomSeed(unsigned long);
+long map(long, long, long, long, long);
+
+#endif // __cplusplus
